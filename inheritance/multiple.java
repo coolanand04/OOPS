96fdd64c2f7be0e7c985b36218514f785b6db8b1
Anand Swaroop Shrivastava <67255043+coolanand04@users.noreply.github.com>
2022-08-15 21:08:42 +0530

    Create multiple.java

---
+++ inheritance/multiple.java
@@ -0,0 +1,35 @@
+class abc{
+    int a = 40;
+    int b = 30;
+}
+
+interface xyz{
+
+}
+
+interface mn{
+
+}
+
+interface ef{
+
+}
+interface op{
+
+}
+
+
+
+
+
+
+class pqr implements xyz, mn, ef, op{
+  
+    
+    public static void main(String[] args)
+    {
+
+    }
+    
+}
+
